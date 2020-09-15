9e5bdee7f4b49c668a3ef6df0ab827077d3bd341
Maximusvin <Maximusvin@gmail.com>
2020-09-15 13:55:29 +0300

    change

--- js/index.js
+++ js/index.js
@@ -12,4 +12,15 @@ $(function () {
 
 const yearOfBirth = 1985;
 const newDate = new Date();
-console.log(newDate.getFullYear() - yearOfBirth);
+const yearOfOld = newDate.getFullYear() - yearOfBirth;
+const ageTag = document.querySelector('.js-age');
+
+function declination(number, titles) {
+  const cases = [2, 0, 1, 1, 1, 2];
+  return titles[number % 100 > 4 && number % 100 < 20 ? 2 : cases[number % 10 < 5 ? number % 10 : 5]];
+}
+
+const title = declination(yearOfOld, [' год', ' года', ' лет']);
+const age = yearOfOld + title;
+
+ageTag.textContent = age;
